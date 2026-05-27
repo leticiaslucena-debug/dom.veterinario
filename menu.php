b4426c4e60b454820fdd57d9b8b9bbc64ba6692b
leticiaslucena-debug <leticia.s.lucena@aluno.senai.br>
2026-05-27 15:43:28 -0300

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,14 @@
+<?php
+
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "sistema_pet";
+
+$conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
+
+if(!$conexao){
+    die("Erro na conexão: " . mysqli_connect_error());
+}
+
+?>
