c3cac90428142eb0b23c1a54b4c0693cdd7e35ee
leticiaslucena-debug <leticia.s.lucena@aluno.senai.br>
2026-06-02 14:05:10 -0300

    Create cadastro.php

---
+++ cadastro.php
@@ -0,0 +1,29 @@
+<?php
+$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = 'alunolab';
+$dbName = 'projeto';
+
+$conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+if($conexao->connect_errno){
+die("Erro de conexão: " .$conexao->connect_error);
+}
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$usuario = $_POST['usuario'];
+$senha = $_POST['senha'];
+
+$sql = "INSERT INTO funcionarios
+(nome, email, usuario, senha)
+
+VALUES
+('$nome', '$email', '$usuario', '$senha')";
+
+if($conexao->query($sql) === TRUE){
+echo "Cadastro solicitado com sucesso!";
+}else{
+echo "Erro ao cadastrar: " . $conexao->error;
+}
+$conexao->close();
+?>
