4c637798400ec8df2c1995e79a92c78f8b68d242
leticiaslucena-debug <leticia.s.lucena@aluno.senai.br>
2026-06-02 15:38:05 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
 $usuario = $_POST['usuario'];
 $senha = $_POST['senha'];
 
-if($usuario == "admin" && $senha == "123456"){
+if($usuario == "Dom" && $senha == "dom123"){
 
     echo "<script>alert('Login realizado com sucesso!'); window.location.href='menu.html';</script>";
 
